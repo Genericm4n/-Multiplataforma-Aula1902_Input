c0c6d56db1da8a5c5712a8aee120bec03dd9ed50
Júlio "genericman" César <47559029+Genericm4n@users.noreply.github.com>
2019-02-19 12:00:44 -0300

    Final da aula

--- Assets/Scripts/ControladorInputTouch.cs
+++ Assets/Scripts/ControladorInputTouch.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class ControladorInputTouch : MonoBehaviour
 {
-    // Use this for initialization
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
     private void Update()
     {
-        int quantidadeToques = Input.touchCount;
-        print(quantidadeToques);
+        //exibir quantidade de toques
+        //int quantidadeToques = Input.touchCount;
+        //print(quantidadeToques);
         //Debug.Log(quantidadeToques);
+
+        //exibir aceleração de dispositivo
+        //var acceleration = Input.acceleration;
+        //print(acceleration);
+
+        //exibir posição do toque usando o ''for''
+        //var position = Input.mousePosition;
+        //print(position);
+
+        //if (quantidadeToques > 0)
+        //{
+        //    for (int indexToque = 0; indexToque < quantidadeToques; indexToque++)
+        //    {
+        //        var touch = Input.GetTouch(indexToque);
+        //        var positionToque = touch.position;
+        //        print(string.Format("Touch: {0} - x: {1}, y:{2}", indexToque, positionToque.x, positionToque.y));
+        //        //print(indexToque);
+        //        //print(positionPrimeiroToque);
+        //    }
+        //}
+
+        //exibir a posição de todos os toques usando o ''foreach''
+        Touch[] touches = Input.touches;
+
+        foreach (Touch touch in touches)
+        {
+            var positionToque = touch.position;
+            print(string.Format("Touch: {0} - TouchPhase:{3} - x: {1}, y:{2}", touch.fingerId, positionToque.x, positionToque.y, touch.phase));
+        }
     }
 }
---
+++ Assets/Scripts/MovimetarCubo.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimetarCubo : MonoBehaviour
+{
+    private float xInicial = 0, yInicial = 0, xFinal = 0, yFinal = 0, xDir = 0, yDir = 0;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var velocidade = 5f;
+        var acceleration = Input.acceleration;
+        transform.Translate(acceleration.x * velocidade * Time.deltaTime, 0, -acceleration.z * velocidade * Time.deltaTime);
+
+        int quantidadeToques = Input.touchCount;
+
+        if (quantidadeToques > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                xInicial = touch.position.x;
+                yInicial = touch.position.y;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                xFinal = touch.position.x;
+                yFinal = touch.position.y;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                xDir = xFinal - xInicial;
+                yDir = yFinal - yInicial;
+
+                xInicial = 0;
+                xFinal = 0;
+                yInicial = 0;
+                yFinal = 0;
+            }
+        }
+        if (xDir > yDir)
+        {
+            if (xDir > 0)
+            {
+                Debug.Log("Direita");
+            }
+            else if (xDir < 0)
+            {
+                Debug.Log("Baixo");
+            }
+        }
+        else
+        {
+            if (yDir > 0)
+            {
+                Debug.Log("Cima");
+            }
+            else if (yDir < 0)
+            {
+                Debug.Log("Esquerda");
+            }
+        }
+
+        xDir = 0;
+        yDir = 0;
+    }
+}
